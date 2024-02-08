b9947a6ca6df3e7cae0589eddd278987c254999c
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-02-07 23:53:15 -0500

    upgrade raw_sample_intervals script

--- scripts/raw_sample_intervals.py
+++ scripts/raw_sample_intervals.py
@@ -1,5 +1,4 @@
 import sys
-import argparse
 import logging
 
 from pycbsdk import cbsdk
@@ -29,26 +28,62 @@ class DummyApp:
     def finish(self):
         b_ts = self._ts > 0
         if np.any(b_ts):
+            mean_itsi = np.mean(np.diff(self._ts[b_ts]))
+            ts_elapsed = self._ts[b_ts][-1] - self._ts[b_ts][0]
+            if mean_itsi < 100:
+                s_elapsed = ts_elapsed / 30_000
+            else:
+                # If timestamps are nanoseconds then mean_itsi should be ~ 33_333
+                s_elapsed = ts_elapsed / 1e9
+            s_elapsed += 1 / 30_000
+            n_samps = np.sum(b_ts)
             print(
-                f"Collected {np.sum(b_ts)} samples in {(self._ts[b_ts][-1] - self._ts[b_ts][0]) / 1e9} s"
+                f"Collected {n_samps} samples in {s_elapsed} s\t({n_samps/s_elapsed:.2f} Hz)."
             )
-            print(np.mean(np.diff(self._ts[b_ts])))
 
 
-def run(duration=11.0, **params_kwargs):
+def main(
+    duration: float = 11.0,
+    smpgroup: int = 6,
+    inst_addr: str = "",
+    inst_port: int = 51002,
+    client_addr: str = "",
+    client_port: int = 51002,
+    recv_bufsize: int = (8 if sys.platform == "win32" else 6) * 1024 * 1024,
+    protocol: str = "4.1",
+    loglevel: str = "debug",
+):
     """
     Run the application:
     - Configure the connection to the nsp
-    - Register a callback that handles the spikes and updates internal state
-    - Run the async chain
-    - On the main thread, render the internal state (print the min-max mean+/-std of the rates every 0.5 seconds).
+    - Create an app, then register it is a callback that receives smp frames and updates internal state
     """
-    params_obj = cbsdk.create_params(**params_kwargs)
+    # Handle logger arguments
+    loglevel = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+    }[loglevel.lower()]
+    logger.setLevel(loglevel)
+
+    # Create connection to the device.
+    params_obj = cbsdk.create_params(
+        inst_addr=inst_addr,
+        inst_port=inst_port,
+        client_addr=client_addr,
+        client_port=client_port,
+        recv_bufsize=recv_bufsize,
+        protocol=protocol,
+    )
     nsp_obj = cbsdk.get_device(params_obj)
-    _ = cbsdk.connect(nsp_obj, startup_sequence=True)
+    if cbsdk.connect(nsp_obj, startup_sequence=True) != 50:
+        logger.error(f"Could not connect to device with params {params_obj}")
+        sys.exit(-1)
     config = cbsdk.get_config(nsp_obj)
 
-    # Disable all channels.
+    # Disable all channels spike and continuous.
+    #  Note: Setting smpgroup=0 will also disable raw.
+    #  Note: Setting any smpgroup also disables filters. :/
     for chid in [
         k
         for k, v in config["channel_types"].items()
@@ -57,8 +92,8 @@ def run(duration=11.0, **params_kwargs):
         _ = cbsdk.set_channel_spk_config(nsp_obj, chid, "enable", False)
         _ = cbsdk.set_channel_config(nsp_obj, chid, "smpgroup", 0)
 
-    # Enable channel 1 raw.
-    _ = cbsdk.set_channel_config(nsp_obj, 1, "smpgroup", 6)
+    # Enable channel 1 at smpgroup.
+    _ = cbsdk.set_channel_config(nsp_obj, 1, "smpgroup", smpgroup)
 
     # Create a dummy app.
     app = DummyApp(duration=duration)
@@ -66,7 +101,7 @@ def run(duration=11.0, **params_kwargs):
     time.sleep(2.0)
 
     # Register callbacks to update the app's state when appropriate packets are received.
-    _ = cbsdk.register_group_callback(nsp_obj, 6, app.handle_frame)
+    _ = cbsdk.register_group_callback(nsp_obj, smpgroup, app.handle_frame)
 
     t_start = time.time()
     try:
@@ -81,65 +116,14 @@ def run(duration=11.0, **params_kwargs):
         _ = cbsdk.disconnect(nsp_obj)
 
 
-def main():
-    # --inst_addr=192.168.137.255 --client_addr=192.168.137.199
-    parser = argparse.ArgumentParser(description="Consume data from (emulated) NSP.")
-    parser.add_argument(
-        "--inst_addr",
-        "-i",
-        type=str,
-        default="",
-        help="ipv4 address to send control packets. Can use subnet. "
-        "Will broadcast if not on Cerebus Subnet."
-        "Use 127.0.0.1 if using with nPlayServer in non-bcast.",
-    )
-    parser.add_argument("--inst_port", type=int, default=51002)
-    parser.add_argument(
-        "--client_addr",
-        "-c",
-        type=str,
-        default="",
-        help="ipv4 address of the adapter we will receive packets on. "
-        "Defaults to INADDR_ANY. If address is provided, assumes Cerebus Subnet.",
-    )
-    parser.add_argument("--client_port", "-p", type=int, default=51002)
-    parser.add_argument(
-        "--recv_bufsize",
-        "-b",
-        type=int,
-        help=f"UDP socket recv buffer size. "
-        f"Default: {(8 if sys.platform == 'win32' else 6) * 1024 * 1024}.",
-    )
-    parser.add_argument("--protocol", type=str, default="4.1")
-    parser.add_argument(
-        "--duration",
-        type=float,
-        default=11.0,
-        help="Intervals will be calculated on data collected over this duration.",
-    )
-    parser.add_argument(
-        "--debug",
-        "-d",
-        help="Print lots of debugging statements",
-        action="store_const",
-        dest="loglevel",
-        const=logging.DEBUG,
-        default=logging.WARNING,
-    )
-    parser.add_argument(
-        "--verbose",
-        "-v",
-        help="Be verbose",
-        action="store_const",
-        dest="loglevel",
-        const=logging.INFO,
-    )
-    args = parser.parse_args()
-    kwargs = vars(args)
-
-    logger.setLevel(kwargs.pop("loglevel"))
-    run(**kwargs)
-
-
 if __name__ == "__main__":
-    main()
+    b_try_no_args = False
+    try:
+        import typer
+
+        typer.run(main)
+    except ModuleNotFoundError:
+        print("Please install typer to use CLI args; using defaults.")
+        b_try_no_args = True
+    if b_try_no_args:
+        main()
